7071224d3748e7e5375a5a12ab362da1cf0e1504
jhseo <junhyeok@jeinsoft.co.kr>
2024-01-05 14:33:32 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # vue_vscode
+
+    - npm install -g @vue/cli
+
+    - Vetur / HTML CSS Support / Vue 3 Snippets / Vue Language Features (Volar) 설치
+        => vue3.x에서는 Vetur 대신 Volar 권장
+        why? => vue2.x까지는 <template>에는 반드시 root element가 하나 있어야 했고, vue3.x에서는 이 제약사항이 풀림
+
+    - vue create vue (yes.then() => Version choice)
+
+    - vue 생성 확인 
+
+    - vue/src/App.vue => html/js/css modify
+
+    - .then(cd vue).then(npm run serve)
+
+        - node_modules : 프로젝트에 쓰는 라이브러리
+        - src          : 소스코드 담는 곳
+        - public       : html 파일, 기타 파일 보관
+        - package.json : 라이브러리 버전, 프로젝트 설정 기록
+        
+# 데이터바인딩
+
+    - template => {{ x }}
+        Because of => No hardcoding && vue auto rendering
+
+    - html 속성 데이터바인딩 => :속성="데이터이름"
+        ex) st = 'color : red' => h1 :style="st" /h1
+
+    - v-for 반복 함수
+
+    - js onClick() => Vue.js @click
+
+    - Vue methods:{
+        <!-- vue에서 사용할 함수 만드는 경우 사용 -->
+
+        함수 안에서 데이터 쓸 땐 this.x / 반드시 this. 를 붙여줘야 변수 가져옴
+    } 
---
+++ vue/src/App.vue
@@ -0,0 +1,139 @@
+<template>
+
+  <div class="black-bg" v-if="모달창열림 == true">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="모달창열림 = false">닫기</button>
+    </div>
+  </div>
+
+  <!-- 상단 Nav 메뉴 -->
+  <div class="menu">
+
+    <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+
+    <!-- <a>Home</a> -->
+    <!-- <a>Products</a> -->
+    <!-- <a>About</a> -->
+  </div>
+
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+<!--   
+  <div v-for="h1 in products" :key="h1">{{ h1 }}
+      <p v-for="가격 in price" :key="가격">{{ 가격 }}</p>
+  </div> 
+  
+  <div v-for="(a,i) in products" :key="i">
+    <h1>{{a}}</h1>
+    <p>{{ price.price1 }} {{ won }}</p>  
+  </div> -->
+
+  <div>
+    <h1 @click="모달창열림 = true">{{ products[0] }}</h1>
+    <p>{{ price.price1 }} {{ won }}</p>
+    <button @click=신고수[0]++>{{ complain }}</button> <span>신고수 : {{ 신고수[0] }}</span>
+    <!-- {{ data 바인딩 }} -->
+  </div>
+
+  <div>
+    <h1>{{ products[1] }}</h1>
+    <p>{{ price.price2 }} {{ won }}</p>  
+    <button @click=신고수[1]++>{{ complain }}</button> <span>신고수 : {{ 신고수[1] }}</span>
+    <!-- {{ data 바인딩 }} -->
+  </div>
+
+  <div>
+    <h1>{{ products[2] }}</h1>
+    <p>{{ price.price3 }} {{ won }}</p>  
+    <button @click=신고수[2]++>{{ complain }}</button> <span>신고수 : {{ 신고수[2] }}</span>
+    <!-- {{ data 바인딩 }} -->
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      won : "만원",
+      complain : "허위신고",
+
+      모달창열림 : false,
+
+      신고수 : [0,0,0],
+
+      price : {
+        price1 : 60,
+        price2 : 100,
+        price3 : "아무거나",
+      },
+
+      products : ['강남', '여의도', '강북'],
+      메뉴들 : ['Home', 'Shop', 'About'],
+
+      if(){
+
+      }
+
+    }
+  },
+  methods : {
+    // vue에서 함수만들고 싶으면 methods:{} 안에 만든다.
+    
+    increase(){
+      this.신고수 += 1;
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  background: black;
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 15px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+</style>
